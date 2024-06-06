9ea599274b7a95134b3732f7d4d07d4da3374abb
hero2324 <143678113+hero2324@users.noreply.github.com>
2024-06-06 09:46:48 +0700

    Update unitTests.py

--- unitTests.py
+++ unitTests.py
@@ -1,216 +1,238 @@
-from Scrabble import *
+import random
+import string
+
+VOWELS = 'aeiou'
+CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
+HAND_SIZE = 7
+
+SCRABBLE_LETTER_VALUES = {
+    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
+}
+
+# -----------------------------------
+# Helper code
+# (you don't need to understand this helper code)
+
+WORDLIST_FILENAME = "words.txt"
+
+
+def load_words():
+    """
+    Returns a list of valid words. Words are strings of lowercase letters.
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print("Loading word list from file...")
+    # in_file: file
+    in_file = open(WORDLIST_FILENAME, 'r')
+    # word_list: list of strings
+    word_list = []
+    for line in in_file:
+        word_list.append(line.strip().lower())
+    print("  ", len(word_list), "words loaded.")
+    return word_list
+
+
+def get_frequency_dict(sequence):
+    """
+    Returns a dictionary where the keys are elements of the sequence
+    and the values are integer counts, for the number of times that
+    an element is repeated in the sequence.
+    sequence: string or list
+    return: dictionary
+    """
+    # freqs: dictionary (element_type -> int)
+    freq = {}
+    for x in sequence:
+        freq[x] = freq.get(x, 0) + 1
+    return freq
+
+
+# (end of helper code)
+# -----------------------------------
 
 #
-# Test code
-# You don't need to understand how this test code works (but feel free to look it over!)
-
-# To run these tests, simply run this file (open up in your IDE, then run the file as normal)
-
-
-def test_get_word_score():
+# Problem #1: Scoring a word
+#
+def get_word_score(word, n):
     """
-    Unit test for get_word_score
+    Returns the score for a word. Assumes the word is a valid word.
+    The score for a word is the sum of the points for letters in the
+    word, multiplied by the length of the word, PLUS 50 points if all n
+    letters are used on the first turn.
+    Letters are scored as in Scrabble; A is worth 1, B is worth 3, C is
+    worth 3, D is worth 2, E is worth 1, and so on (see SCRABBLE_LETTER_VALUES)
+    word: string (lowercase letters)
+    n: integer (HAND_SIZE; i.e., hand size required for additional points)
+    returns: int >= 0
     """
-    failure = False
-    # dictionary of words and scores
-    words = {("", 7): 0, ("it", 7): 4, ("was", 7): 18, ("scored", 7): 54,
-             ("waybill", 7): 155, ("outgnaw", 7): 127, ("fork", 7): 44, ("fork", 4): 94}
-    for (word, n) in words.keys():
-        score = get_word_score(word, n)
-        if score != words[(word, n)]:
-            print("FAILURE: test_get_word_score()")
-            print("\tExpected", words[(word, n)], "points but got '" +
-                  str(score) + "' for word '" + word + "', n=" + str(n))
-            failure = True
-    if not failure:
-        print("SUCCESS: test_get_word_score()")
-
-# end of test_get_word_score
+    # TO DO ... <-- Remove this comment when you code this function
 
 
-def test_update_hand():
+#
+# Problem #2: Make sure you understand how this function works and what it does!
+#
+def display_hand(hand):
     """
-    Unit test for update_hand
+    Displays the letters currently in the hand.
+    For example:
+    >>> display_hand({'a':1, 'x':2, 'l':3, 'e':1})
+    Should print out something like:
+       a x x l l l e
+    The order of the letters is unimportant.
+    hand: dictionary (string -> int)
     """
-    # test 1
-    hand_orig = {'a': 1, 'q': 1, 'l': 2, 'm': 1, 'u': 1, 'i': 1}
-    hand_copy = hand_orig.copy()
-    word = "quail"
+    for letter in hand.keys():
+        for j in range(hand[letter]):
+            print(letter, end=" ")       # print all on the same line
+    print()                             # print an empty line
 
-    hand2 = update_hand(hand_copy, word)
-    expected_hand1 = {'l': 1, 'm': 1}
-    expected_hand2 = {'a': 0, 'q': 0, 'l': 1, 'm': 1, 'u': 0, 'i': 0}
-    if hand2 != expected_hand1 and hand2 != expected_hand2:
-        print("FAILURE: test_update_hand('" + word + "', " + str(hand_orig) + ")")
-        print("\tReturned: ", hand2, "\n\t-- but expected:",
-              expected_hand1, "or", expected_hand2)
-
-        return  # exit function
-    if hand_copy != hand_orig:
-        print("FAILURE: test_update_hand('" + word + "', " + str(hand_orig) + ")")
-        print("\tOriginal hand was", hand_orig)
-        print("\tbut implementation of update_hand mutated the original hand!")
-        print("\tNow the hand looks like this:", hand_copy)
-
-        return  # exit function
-
-    # test 2
-    hand_orig = {'e': 1, 'v': 2, 'n': 1, 'i': 1, 'l': 2}
-    hand_copy = hand_orig.copy()
-    word = "evil"
-
-    hand2 = update_hand(hand_copy, word)
-    expected_hand1 = {'v': 1, 'n': 1, 'l': 1}
-    expected_hand2 = {'e': 0, 'v': 1, 'n': 1, 'i': 0, 'l': 1}
-    if hand2 != expected_hand1 and hand2 != expected_hand2:
-        print("FAILURE: test_update_hand('" + word + "', " + str(hand_orig) + ")")
-        print("\tReturned: ", hand2, "\n\t-- but expected:",
-              expected_hand1, "or", expected_hand2)
-
-        return  # exit function
-
-    if hand_copy != hand_orig:
-        print("FAILURE: test_update_hand('" + word + "', " + str(hand_orig) + ")")
-        print("\tOriginal hand was", hand_orig)
-        print("\tbut implementation of update_hand mutated the original hand!")
-        print("\tNow the hand looks like this:", hand_copy)
-
-        return  # exit function
-
-    # test 3
-    hand_orig = {'h': 1, 'e': 1, 'l': 2, 'o': 1}
-    hand_copy = hand_orig.copy()
-    word = "hello"
-
-    hand2 = update_hand(hand_copy, word)
-    expected_hand1 = {}
-    expected_hand2 = {'h': 0, 'e': 0, 'l': 0, 'o': 0}
-    if hand2 != expected_hand1 and hand2 != expected_hand2:
-        print("FAILURE: test_update_hand('" + word + "', " + str(hand_orig) + ")")
-        print("\tReturned: ", hand2, "\n\t-- but expected:",
-              expected_hand1, "or", expected_hand2)
-
-        return  # exit function
-
-    if hand_copy != hand_orig:
-        print("FAILURE: test_update_hand('" + word + "', " + str(hand_orig) + ")")
-        print("\tOriginal hand was", hand_orig)
-        print("\tbut implementation of update_hand mutated the original hand!")
-        print("\tNow the hand looks like this:", hand_copy)
-
-        return  # exit function
-
-    print("SUCCESS: test_update_hand()")
-
-# end of test_update_hand
+#
+# Problem #2: Make sure you understand how this function works and what it does!
+#
 
 
-def test_is_valid_word(word_list):
+def deal_hand(n):
     """
-    Unit test for is_valid_word
+    Returns a random hand containing n lowercase letters.
+    At least n/3 the letters in the hand should be VOWELS.
+    Hands are represented as dictionaries. The keys are
+    letters and the values are the number of times the
+    particular letter is repeated in that hand.
+    n: int >= 0
+    returns: dictionary (string -> int)
     """
-    failure = False
-    # test 1
-    word = "hello"
-    hand_orig = get_frequency_dict(word)
-    hand_copy = hand_orig.copy()
+    hand = {}
+    num_vowels = n // 3
 
-    if not is_valid_word(word, hand_copy, word_list):
-        print("FAILURE: test_is_valid_word()")
-        print("\tExpected True, but got False for word: '" +
-              word + "' and hand:", hand_orig)
+    for i in range(num_vowels):
+        x = VOWELS[random.randrange(0, len(VOWELS))]
+        hand[x] = hand.get(x, 0) + 1
 
-        failure = True
+    for i in range(num_vowels, n):
+        x = CONSONANTS[random.randrange(0, len(CONSONANTS))]
+        hand[x] = hand.get(x, 0) + 1
 
-    # Test a second time to see if word_list or hand has been modified
-    if not is_valid_word(word, hand_copy, word_list):
-        print("FAILURE: test_is_valid_word()")
+    return hand
 
-        if hand_copy != hand_orig:
-            print("\tTesting word", word,
-                  "for a second time - be sure you're not modifying hand.")
-            print("\tAt this point, hand ought to be",
-                  hand_orig, "but it is", hand_copy)
-
-        else:
-            print("\tTesting word", word,
-                  "for a second time - have you modified word_list?")
-            word_in_wl = word in word_list
-            print("The word", word, "should be in word_list - is it?", word_in_wl)
-
-        print("\tExpected True, but got False for word: '" +
-              word + "' and hand:", hand_copy)
-
-        failure = True
-
-    # test 2
-    hand = {'r': 1, 'a': 3, 'p': 2, 'e': 1, 't': 1, 'u': 1}
-    word = "rapture"
-
-    if is_valid_word(word, hand, word_list):
-        print("FAILURE: test_is_valid_word()")
-        print("\tExpected False, but got True for word: '" +
-              word + "' and hand:", hand)
-
-        failure = True
-
-    # test 3
-    hand = {'n': 1, 'h': 1, 'o': 1, 'y': 1, 'd': 1, 'w': 1, 'e': 2}
-    word = "honey"
-
-    if not is_valid_word(word, hand, word_list):
-        print("FAILURE: test_is_valid_word()")
-        print("\tExpected True, but got False for word: '" +
-              word + "' and hand:", hand)
-
-        failure = True
-
-    # test 4
-    hand = {'r': 1, 'a': 3, 'p': 2, 't': 1, 'u': 2}
-    word = "honey"
-
-    if is_valid_word(word, hand, word_list):
-        print("FAILURE: test_is_valid_word()")
-        print("\tExpected False, but got True for word: '" +
-              word + "' and hand:", hand)
-
-        failure = True
-
-    # test 5
-    hand = {'e': 1, 'v': 2, 'n': 1, 'i': 1, 'l': 2}
-    word = "evil"
-
-    if not is_valid_word(word, hand, word_list):
-        print("FAILURE: test_is_valid_word()")
-        print("\tExpected True, but got False for word: '" +
-              word + "' and hand:", hand)
-
-        failure = True
-
-    # test 6
-    word = "even"
-
-    if is_valid_word(word, hand, word_list):
-        print("FAILURE: test_is_valid_word()")
-        print("\tExpected False, but got True for word: '" +
-              word + "' and hand:", hand)
-        print("\t(If this is the only failure, make sure is_valid_word() isn't mutating its inputs)")
-
-        failure = True
-
-    if not failure:
-        print("SUCCESS: test_is_valid_word()")
+#
+# Problem #2: Update a hand by removing letters
+#
 
 
-word_list = load_words()
-print("----------------------------------------------------------------------")
-print("Testing get_word_score...")
-test_get_word_score()
-print("----------------------------------------------------------------------")
-print("Testing update_hand...")
-test_update_hand()
-print("----------------------------------------------------------------------")
-print("Testing is_valid_word...")
-test_is_valid_word(word_list)
-print("----------------------------------------------------------------------")
-print("All done!")
+def update_hand(hand, word):
+    """
+    Assumes that 'hand' has all the letters in word.
+    In other words, this assumes that however many times
+    a letter appears in 'word', 'hand' has at least as
+    many of that letter in it. 
+    Updates the hand: uses up the letters in the given word
+    and returns the new hand, without those letters in it.
+    Has no side effects: does not modify hand.
+    word: string
+    hand: dictionary (string -> int)    
+    returns: dictionary (string -> int)
+    """
+    # TO DO ... <-- Remove this comment when you code this function
+
+
+#
+# Problem #3: Test word validity
+#
+def is_valid_word(word, hand, word_list):
+    """
+    Returns True if word is in the word_list and is entirely
+    composed of letters in the hand. Otherwise, returns False.
+    Does not mutate hand or word_list.
+    word: string
+    hand: dictionary (string -> int)
+    word_list: list of lowercase strings
+    """
+    # TO DO ... <-- Remove this comment when you code this function
+
+
+#
+# Problem #4: Playing a hand
+#
+
+def calculate_hand_len(hand):
+    """ 
+    Returns the length (number of letters) in the current hand.
+    hand: dictionary (string-> int)
+    returns: integer
+    """
+    # TO DO... <-- Remove this comment when you code this function
+
+
+def play_hand(hand, word_list, n):
+    """
+    Allows the user to play the given hand, as follows:
+    * The hand is displayed.
+    * The user may input a word or a single period (the string ".") 
+      to indicate they're done playing
+    * Invalid words are rejected, and a message is displayed asking
+      the user to choose another word until they enter a valid word or "."
+    * When a valid word is entered, it uses up letters from the hand.
+    * After every valid word: the score for that word is displayed,
+      the remaining letters in the hand are displayed, and the user
+      is asked to input another word.
+    * The sum of the word scores is displayed when the hand finishes.
+    * The hand finishes when there are no more unused letters or the user
+      inputs a "."
+      hand: dictionary (string -> int)
+      word_list: list of lowercase strings
+      n: integer (HAND_SIZE; i.e., hand size required for additional points)
+    """
+    # BEGIN PSEUDOCODE <-- Remove this comment when you code this function; do your coding within the pseudocode (leaving those comments in-place!)
+    # Keep track of the total score
+
+    # As long as there are still letters left in the hand:
+
+    # Display the hand
+
+    # Ask user for input
+
+    # If the input is a single period:
+
+    # End the game (break out of the loop)
+
+    # Otherwise (the input is not a single period):
+
+    # If the word is not valid:
+
+    # Reject invalid word (print a message followed by a blank line)
+
+    # Otherwise (the word is valid):
+
+    # Tell the user how many points the word earned, and the updated total score, in one line followed by a blank line
+
+    # Update the hand
+
+    # Game is over (user entered a '.' or ran out of letters), so tell user the total score
+
+
+#
+# Problem #5: Playing a game
+#
+
+def play_game(word_list):
+    """
+    Allow the user to play an arbitrary number of hands.
+    1) Asks the user to input 'n' or 'r' or 'e'.
+      * If the user inputs 'n', let the user play a new (random) hand.
+      * If the user inputs 'r', let the user play the last hand again.
+      * If the user inputs 'e', exit the game.
+      * If the user inputs anything else, tell them their input was invalid.
+    2) When done playing the hand, repeat from step 1    
+    """
+    # TO DO ... <-- Remove this comment when you code this function
+    # <-- Remove this line when you code the function
+    print("play_game not yet implemented.")
+
+
+#
+# Build data structures used for entire session and play game
+#
+if __name__ == '__main__':
+    word_list = load_words()
+    play_game(word_list)
